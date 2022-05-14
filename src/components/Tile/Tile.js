dda163036f47c225c37e06c27d1f305f9a046a50
Anunay Aatipamula <anunay1997@gmail.com>
2022-05-14 17:34:08 +0530

    module-2

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 import './Tile.css'
 
-const Tile = () => {
+const Tile = (props) => {
 
+  tileColor = (props.selected == true) || (props.matched == true) ? {backgroundColor : props.color} : null;
   return (
     <div className='Tile'>
+      {(props.selected == true) || (props.matched == true) ? <props.svg/> : null}
     </div>
   )
 }
